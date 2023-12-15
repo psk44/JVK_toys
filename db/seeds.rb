adcfefd7fe215831fa66d1308ddb552b2fa6b033
pete <kouyoumdjianps@masters.edu>
2023-12-15 04:54:59 +0000

    new and improved seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,47 @@
+# Create Toy Types
+toy_types = ['Type 1', 'Type 2', 'Type 3'].map do |type_name|
+  ToyType.find_or_create_by(name: type_name, description: "#{type_name} description")
+end
+
 # Create Suppliers
 supplier1 = Supplier.create(name: "Supplier One", email: "supplier1@example.com", phone: "1234567890", address: "123 Supplier St, Supplier City")
 supplier2 = Supplier.create(name: "Supplier Two", email: "supplier2@example.com", phone: "0987654321", address: "456 Supplier Ave, Supplier Town")
 supplier3 = Supplier.create(name: "Supplier Three", email: "supplier3@example.com", phone: "1122334455", address: "789 Supplier Blvd, Supplier Village")
 
-# Create Toy Types 
-toy_type1 = ToyType.create(name: "Type One", description: "Description for Type One")
-toy_type2 = ToyType.create(name: "Type Two", description: "Description for Type Two")
-toy_type3 = ToyType.create(name: "Type Three", description: "Description for Type Three")
+# Create Toys with associated Inventory, Sales, and Ratings
+5.times do |i|
+  toy = Toy.find_or_create_by(
+    name: "Toy #{i + 1}",
+    description: "This is the description for Toy #{i + 1}.",
+    price: 10,
+    toy_type: toy_types.sample # Randomly assign one of the toy types
+  )
 
-# Create Toys and associate with Types and Suppliers
-toy1 = Toy.create(name: "Toy One", description: "Description for Toy One", price: 5, toy_type: toy_type1)
-toy2 = Toy.create(name: "Toy Two", description: "Description for Toy Two", price: 5, toy_type: toy_type2)
-toy3 = Toy.create(name: "Toy Three", description: "Description for Toy Three", price: 5, toy_type: toy_type3)
+  inventory = Inventory.find_or_create_by(
+    toy: toy,
+    quantity: 50,
+    low_inventory_threshold: 5
+  )
 
-# Associating toys with suppliers
-toy1.suppliers << supplier1
-toy2.suppliers << supplier2
-toy3.suppliers << supplier3
+  # Create two sales with random quantities and times for each toy
+  2.times do
+    quantity_sold = rand(1..10)
+    Sale.create(
+      toy: toy,
+      quantity_sold: quantity_sold,
+      sale_date: rand(1.year).seconds.ago
+    )
+  end
 
-# Create Inventories
-Inventory.create(toy: toy1, quantity: 10, low_inventory_threshold: 2)
-Inventory.create(toy: toy2, quantity: 10, low_inventory_threshold: 2)
-Inventory.create(toy: toy3, quantity: 10, low_inventory_threshold: 2)
+  # Create two ratings each toy
+  2.times do
+    Rating.create(
+      toy: toy,
+      score: rand(1..5),
+      review: ["Great toy!", "Loved by kids!", "Very durable.", "Hours of fun!", "Educational and entertaining."].sample
+    )
+  end
+end
 
-puts "Created 3 toys, 3 suppliers, and set inventory for each toy."
+puts "Created #{Toy.count} toys."
+puts "Created #{Supplier.count} suppliers."
