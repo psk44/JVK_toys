3efdea61816ccca497613451022fc5126d5938f2
pete <kouyoumdjianps@masters.edu>
2023-12-15 05:07:11 +0000

    adjusted sales so that revenue is calculated automatically at sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,8 +1,13 @@
 class Sale < ApplicationRecord
   belongs_to :toy
   after_create :decrement_inventory
+  before_save :set_total_price
 
   private
+
+  def set_total_price
+    self.total_price = toy.price * quantity_sold
+  end
 
   def decrement_inventory
     toy.inventory.decrement!(:quantity, self.quantity_sold)
